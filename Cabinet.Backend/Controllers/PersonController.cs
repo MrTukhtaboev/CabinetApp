bec56b2f57ead8535deae87e37f420c191426d0d
Mukhammadkarim Tukhtaboyev <75162831+MrTukhtaboyev@users.noreply.github.com>
2021-07-25 17:34:43 +0500

    rtjhetryj

--- Cabinet.Backend/Controllers/PersonController.cs
+++ Cabinet.Backend/Controllers/PersonController.cs
@@ -40,7 +40,7 @@ namespace Cabinet.Backend.Controllers
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody]Person person)
+        public IActionResult Post(Person person)
         {
             CabinetDb.Persons.Add(person);
             CabinetDb.SaveChanges();
